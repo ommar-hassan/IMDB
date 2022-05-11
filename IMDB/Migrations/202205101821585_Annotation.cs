b0d1e773222a1d3b2cb9a047478e8bba7dbd2bb0
ommar-hassan <102484131+ommar-hassan@users.noreply.github.com>
2022-05-11 22:55:01 +0200

    conflicts

---
+++ IMDB/Migrations/202205101821585_Annotation.cs
@@ -0,0 +1,16 @@
+﻿namespace IMDB.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Annotation : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
