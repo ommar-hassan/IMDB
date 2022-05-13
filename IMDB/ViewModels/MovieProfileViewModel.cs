949716485d7791855b19ab6ac47ccd0410a259e6
ommar-hassan <102484131+ommar-hassan@users.noreply.github.com>
2022-05-14 00:44:09 +0200

    MovieProfile view model

---
+++ IMDB/ViewModels/MovieProfileViewModel.cs
@@ -0,0 +1,19 @@
+﻿using IMDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMDB.ViewModels
+{
+    public class MovieProfileViewModel
+    {
+        public Movie Movie { get; set; }
+
+        public IEnumerable<Actor> Actors { get; set; }
+
+        public IEnumerable<MovieActor> MovieActor { get; set; }
+
+        public Director Director { get; set; }
+    }
+}
