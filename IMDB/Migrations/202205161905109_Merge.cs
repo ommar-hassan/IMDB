56aa25a617a4f15bf3dc12fc8a376594844c0aa3
ommar-hassan <102484131+ommar-hassan@users.noreply.github.com>
2022-05-17 00:58:10 +0200

    update database

---
+++ IMDB/Migrations/202205161905109_Merge.cs
@@ -0,0 +1,24 @@
+﻿namespace IMDB.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Merge : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "Email", c => c.String(nullable: false));
+            AlterColumn("dbo.Users", "FirstName", c => c.String(nullable: false));
+            AlterColumn("dbo.Users", "LastName", c => c.String(nullable: false));
+            AlterColumn("dbo.Users", "Password", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Users", "Password", c => c.String());
+            AlterColumn("dbo.Users", "LastName", c => c.String());
+            AlterColumn("dbo.Users", "FirstName", c => c.String());
+            DropColumn("dbo.Users", "Email");
+        }
+    }
+}
